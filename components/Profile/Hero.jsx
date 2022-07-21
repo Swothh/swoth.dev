e8bcae4ef2b4aa777a98e3b80b6e2b3b756191a3
Swôth <56405862+Swothh@users.noreply.github.com>
2022-07-22 02:37:51 +0300

    Delete components/Profile directory

--- components/Profile/Hero.jsx
+++
@@ -1,8 +0,0 @@
-export default function Hero() {
-    return (
-        <>
-            <h1 className="text-3xl text-white font-bold text-center">Profile</h1>
-            <h6 className="uppercase text-gray-400 text-sm text-center mb-3">You can edit your profile.</h6>
-        </>
-    );
-};
