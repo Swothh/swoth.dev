770226a3644a7c05c273fa2bead4ee1156d67ee0
Swôth <56405862+Swothh@users.noreply.github.com>
2024-03-31 12:48:55 +0300

    Delete lib directory

--- lib/swr.jsx
+++
@@ -1,6 +0,0 @@
-import fetch from 'isomorphic-unfetch';
-import useSWR from 'swr';
-
-export default function SWR(url, interval = 3000) {
-    return useSWR(url, href => fetch(href).then(res => res.json()), { refreshInterval: interval });
-};
