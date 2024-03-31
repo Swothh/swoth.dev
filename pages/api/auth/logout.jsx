f6419652219c292df633ecad54a7492a5cedf94d
Swôth <56405862+Swothh@users.noreply.github.com>
2024-03-31 12:49:23 +0300

    Delete pages directory

--- pages/api/auth/logout.jsx
+++
@@ -1,7 +0,0 @@
-import Logout from '../../../lib/session.jsx';
-
-export default Logout(async (req, res, session) => {
-    if (req.method !== 'POST') return res.status(200).json({ success: false, message: 'Method not allowed!' });
-    await req.session.destroy();
-    res.status(200).json({ success: true, message: 'You have successfully logged out!' });
-});
--- pages/api/util/activity.jsx
+++
@@ -1,7 +0,0 @@
-import fetch from 'isomorphic-unfetch';
-
-export default async function Activity(req, res) {
-    res.json(
-        await (await fetch('https://linkcord.js.org/api/v3/user/451444721089380373')).json()
-    );
-};
